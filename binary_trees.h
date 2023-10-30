7b5c9d3904d87e56a41d26ffe7a670e26f81dbf9
Chris Adongo <119563006+ChrisAdongo@users.noreply.github.com>
2023-10-30 18:06:54 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -28,7 +28,8 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /* PROTOTYPES FOR PRINTING */
-void binary_tree_print(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *);
+/* void binary_tree_print(const binary_tree_t *tree); */
 
 /* PROTOTYPES */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
